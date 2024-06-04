2bd64e29360d50c8e9f497cc1345b4a932ad8d36
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-05 00:39:29 +0300

    update decorators: short, unsigned, int, float

--- src/packets/structs/decorators.ts
+++ src/packets/structs/decorators.ts
@@ -7,18 +7,39 @@ export const char = (length: number) => {
     return Reflect.metadata(key, { type: 'char', length });
 }
 
+/* 1-byte unsigned integer */
 export const byte = () => {
     return Reflect.metadata(key, { type: 'byte' });
 }
 
+/* 2-byte unsigned integer */
 export const word = () => {
     return Reflect.metadata(key, { type: 'word' });
+}
+
+/* 2-byte signed integer */
+export const short = () => {
+    return Reflect.metadata(key, { type: 'short' });
+}
+
+/* 4-byte unsigned integer */
+export const unsigned = () => {
+    return Reflect.metadata(key, { type: 'unsigned' });
+}
+
+/* 4-byte signed integer */
+export const int = () => {
+    return Reflect.metadata(key, { type: 'int' });
+}
+
+/* 4-byte float */
+export const float = () => {
+    return Reflect.metadata(key, { type: 'float' });
 }
 
 export const getFormat = (target: any, propertyKey: string) => {
     return Reflect.getMetadata(key, target, propertyKey);
 }
-
 
 class PacketHandler {
     all: { [name: string]: new() => Sendable | Receivable } = {};
