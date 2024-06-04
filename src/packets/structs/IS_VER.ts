81555efefd0fbd2d930ce665b711eeb6cf8c2d75
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-05 02:04:24 +0300

    IS_VER: readonly for ReqI

--- src/packets/structs/IS_VER.ts
+++ src/packets/structs/IS_VER.ts
@@ -6,7 +6,7 @@ import { PacketType } from "packets/types/PacketType.js";
 export class IS_VER extends Receivable {
     @byte() readonly Size = 20;
     @byte() readonly Type = PacketType.ISP_VER;
-    @byte() ReqI = 0;
+    @byte() readonly ReqI = 0;
     @byte() readonly Zero = 0;
 
     @char(8) Version = '';
