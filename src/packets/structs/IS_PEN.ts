c7ea6c29a45e41143f188a20aefa96d5aeeb9156
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-05 03:59:47 +0300

    IS_PEN, IS_PFL, IS_PIT, IS_PLA, IS_PLC

---
+++ src/packets/structs/IS_PEN.ts
@@ -0,0 +1,18 @@
+import { Receivable } from "packets/utilities/index.js";
+import { define, byte } from "./decorators.js";
+import { PacketType } from "packets/types/PacketType.js";
+import { PenaltyValue } from "packets/enums/PenaltyValue.js";
+import { PenaltyReason } from "packets/enums/PenaltyReason.js";
+
+@define
+export class IS_PEN extends Receivable {
+    @byte() readonly Size = 8;
+    @byte() readonly Type = PacketType.ISP_PEN;
+    @byte() readonly ReqI = 0;
+    @byte() PLID = 0;
+
+    @byte() OldPen: PenaltyValue = 0;
+    @byte() NewPen: PenaltyValue = 0;
+    @byte() Reason: PenaltyReason = 0;
+    @byte() private readonly Sp3 = 0;
+}
---
+++ src/packets/structs/IS_PLA.ts
@@ -0,0 +1,17 @@
+import { Receivable } from "packets/utilities/index.js";
+import { define, byte } from "./decorators.js";
+import { PacketType } from "packets/types/PacketType.js";
+import { PitLaneFact } from "packets/enums/PitLaneFact.js";
+
+@define
+export class IS_PLA extends Receivable {
+    @byte() readonly Size = 8;
+    @byte() readonly Type = PacketType.ISP_PLA;
+    @byte() readonly ReqI = 0;
+    @byte() PLID = 0;
+
+    @byte() Fact: PitLaneFact = 0;
+    @byte() private readonly Sp1 = 0;
+    @byte() private readonly Sp2 = 0;
+    @byte() private readonly Sp3 = 0;
+}
