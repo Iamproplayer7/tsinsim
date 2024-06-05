b249b1bbb080ee14f5703fa0bc6367c41272773a
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-05 08:50:46 +0300

    add leftover packets and structs

---
+++ src/packets/enums/UCOAction.ts
@@ -0,0 +1,6 @@
+export enum UCOAction {
+	UCO_CIRCLE_ENTER,	// entered a circle
+	UCO_CIRCLE_LEAVE,	// left a circle
+	UCO_CP_FWD,			// crossed cp in forward direction
+	UCO_CP_REV,			// crossed cp in reverse direction
+};
