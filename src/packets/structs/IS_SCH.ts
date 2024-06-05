b113b70b83b0e814dbf1dfa4dcba60432d230965
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-05 04:29:07 +0300

    IS_CPP, IS_MOD, IS_MST, IS_SCC, IS_SCH, IS_SFP

---
+++ src/packets/structs/IS_SCH.ts
@@ -0,0 +1,21 @@
+import { Sendable } from "packets/utilities/index.js";
+import { define, byte } from "./decorators.js";
+import { PacketType } from "packets/types/PacketType.js";
+
+@define
+export class IS_SCH extends Sendable {
+    constructor(options?: {}) { 
+        super(); 
+        Object.assign(this, options);
+    }
+
+    @byte() readonly Size = 8;
+    @byte() readonly Type = PacketType.ISP_SCH;
+    @byte() readonly ReqI = 0;
+    @byte() readonly Zero = 0;
+
+    @byte() CharB = 0;
+    @byte() Flags: 0 | 1 = 0;
+    @byte() private readonly Spare2 = 0;
+    @byte() private readonly Spare3 = 0;
+}
