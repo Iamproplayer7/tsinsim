2300b03ec29ceebafaf1fa141b631889f88567dc
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-05 03:57:55 +0300

    IS_BFN

---
+++ src/packets/structs/IS_BFN.ts
@@ -0,0 +1,21 @@
+import { Sendable } from "packets/utilities/index.js";
+import { define, byte } from "./decorators.js";
+import { PacketType } from "packets/types/PacketType.js";
+
+@define
+export class IS_BFN extends Sendable {
+    constructor(options?: {}) { 
+        super(); 
+        Object.assign(this, options);
+    }
+
+    @byte() readonly Size = 8;
+    @byte() readonly Type = PacketType.ISP_BFN;
+    @byte() readonly ReqI = 0;
+    @byte() readonly Zero = 0;
+
+    @byte() UCID = 0;
+    @byte() ClickID = 0;
+    @byte() ClickMAX = 0;
+    @byte() Inst = 0;
+}
