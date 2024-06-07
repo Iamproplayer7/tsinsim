62b0e1b8faa83eadd0a5bb6870ac65caced0c8b1
Iamproplayer7 <iamproplayer7@gmail.com>
2024-06-08 01:32:42 +0300

    add Events.on(() => {})

--- src/utilities/events.ts
+++ src/utilities/events.ts
@@ -1,21 +1,28 @@
 import { PacketType } from "../packets/types/index.js";
 
-type Name = PacketType | 'connect' | 'disconnect' | 'connected';
-type Callback = (data: any) => void;
+type Name = PacketType | 'connect' | 'disconnect' | 'connected' | 'global';
+type Callback_1 = (data: any, name: Name) => void;
+type Callback_2 = (name: Name, data: any) => void;
 
 export class Events {
-    private all: { name: Name, callback: Callback}[] = [];
+    private all: { name: Name, callback: Callback_1 | Callback_2}[] = [];
 
-    on(name: Name, callback: Callback) {
-        this.all.push({ name, callback });
+    on(name: Name | Callback_2, callback: Callback_1 | null = null) {
+        if(typeof name == 'function') {
+            this.all.push({ name: 'global', callback: name });
+        }
+        else {
+            if(callback) {
+                this.all.push({ name, callback });
+            }
+        }
     }
 
     fire(name: Name, data?: any) {
-        const events = this.all.filter((event) => event.name === name);
-        if(events.length > 0) {
-            for(const event of events) {
-                event.callback(data);
-            }
+        const events = this.all.filter((event) => event.name === name || event.name === 'global');
+        for(const event of events) {
+            if(event.name === 'global') event.callback(name, data);
+            else event.callback(data, event.name);
         }
     }
 }
