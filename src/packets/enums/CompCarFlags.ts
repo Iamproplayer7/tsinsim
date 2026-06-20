d7d54d471f164f76350be244b2c9e897effdf5e6
Iamproplayer7 <iamproplayer7@gmail.com>
2026-06-20 17:45:16 +0300

    add CCI_RETIRED

--- src/packets/enums/CompCarFlags.ts
+++ src/packets/enums/CompCarFlags.ts
@@ -2,6 +2,7 @@ export enum CompCarFlags {
     CCI_BLUE	=	1,		// this car is in the way of a driver who is a lap ahead 
     CCI_YELLOW	=	2,		// this car is slow or stopped and in a dangerous place
     CCI_OOB	    =   4,		// this car is outside the path
+    CCI_RETIRED	=   8,		// this car has been retired
     CCI_LAG		=	32,		// this car is lagging (missing or delayed position packets)
     CCI_FIRST	=	64,		// this is the first compcar in this set of MCI packets
     CCI_LAST	=	128		// this is the last compcar in this set of MCI packets
